2a63b3ccd7c96d1ce800abce1f9a04f35a9b3f99
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2018-02-17 16:37:49 +0000

    table

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
 import './style';
 import { Component } from 'preact';
 
-export default class App extends Component {
+class App extends Component {
+
+	state = {
+	  coffees: []
+	}
+  
+	async componentDidMount() {
+	  const coffees = await fetch('http://www.mocky.io/v2/5a88237130000080007f93e0')
+			.then((response) => response.json());
+	  this.setState({ coffees });
+	}
+  
 	render() {
-		return (
-			<div>
-				<h1>Hello, World!</h1>
+	  return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">React</h1>
+				</header>
+				<CoffeeTable coffees={this.state.coffees} />
 			</div>
-		);
+	  );
 	}
 }
+  
+const CoffeeTable = ({ coffees }) => (
+	  <table style={StyleSheet.table}>
+		<tbody>
+			<tr style={StyleSheet.th}>
+				<th>Name</th>
+				<th>Price</th>
+			</tr>
+			{coffees.map((coffee, i ) => (
+				<tr key={i}>
+					<td>{coffee.name}</td>
+					<td>{coffee.price}</td>
+				</tr>
+			))}
+		</tbody>
+	  </table>
+);
+  
+const StyleSheet = {
+	table: {
+		fontFamily: 'arial',
+		borderCollapse: 'collapse',
+		width: '100%'
+	},
+	th: {
+		border: 1,
+		borderStyle: 'solid',
+		borderColor: '#dddddd',
+		textAlign: 'left',
+		padding: 8
+	}
+};
+  
+export default App;
